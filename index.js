7d6516bea0a834247667819d8d836ec7f2e33feb
Jackson Choyce <jacksonchoyce@gmail.com>
2022-07-28 19:33:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
 const token = process.env.THREAD_CREATOR_DISCORD_TOKEN;
 
 const monitoredChannels = [
-	'videos'
+	"videos",
+	"ai-news"
 ];
 
-const client = new Client({ 
+const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent
-	] 
+		GatewayIntentBits.MessageContent,
+	],
 });
 
-client.once('ready', () => {
-	console.log('Thread Creator is ready to rumble...');
+client.once("ready", () => {
+	console.log("Thread Creator is ready to rumble...");
+	client.user.setActivity("with threads", { type: "PLAYING" });
 });
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
+	if (message.author.bot) return;
+
 	let channelName = client.channels.cache.get(message.channelId).name;
-	
-	if(!monitoredChannels.includes(channelName))
-		return;
+	if (!monitoredChannels.includes(channelName)) return;
 
-	try{
+	try {
+		// check if message is not a link
+		if (
+			!message.content.match(
+				/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+			)
+		) {
+			// delete the message
+			await message.delete();
+			// respond with a warning
+			let msg = await message.channel.send(
+				`${message.author} You can only send links here!`
+			);
+			// delete message after 5 seconds
+			setTimeout(() => {
+				msg.delete();
+			}, 5000);
+			return;
+		}
+		let thread_name = message.author.username + "'s video: discussion";
 		await message.startThread({
-			name: message.content,
-			type: 'GUILD_PUBLIC_THREAD',
-			autoArchiveDuration: 60
+			name: thread_name,
+			type: "GUILD_PUBLIC_THREAD",
+			autoArchiveDuration: 60,
 		});
-	}
-	catch(error){
+	} catch (error) {
 		console.error(error);
 	}
-	
 });
 
 client.login(token);
