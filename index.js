efeba0b0947476a4275b4883a14bebc53eb67935
Jackson Choyce <58273860+neverabsolute@users.noreply.github.com>
2022-08-31 22:09:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const token = process.env.THREAD_CREATOR_DISCORD_TOKEN;
 
 const monitoredChannels = [
 	"videos",
-	"ai-news"
+	"ai-news",
+	"frontend-dev",
+	"backend-dev",
+	"mobile-dev",
+	"applied-ai",
+	"gpt",
+	"computer-vision",
+	"nft-art",
+	"product-management",
 ];
 
 const client = new Client({
